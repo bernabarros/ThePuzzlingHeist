bf30fa0c3d707f890c970e8c07497c77c8962f92
bernabarros <bernardobarros.work@gmail.com>
2025-11-23 00:21:01 +0000

    Add animation transition to camera switches, create script for camera puzzle

---
+++ Assets/Scripts/CameraPuzzle/CameraPuzzleManager.cs
@@ -0,0 +1,15 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CameraPuzzleManager : MonoBehaviour
+{
+    [SerializeField] public GameObject[] combination;
+    //public int[] correctCombination = {1,0,0,1,0,1,1,0};
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
