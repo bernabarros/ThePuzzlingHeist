1161e18f9886c76875093964c2086c416eb03b57
bernabarros <bernardobarros.work@gmail.com>
2025-12-03 00:08:17 +0000

    Add camera diagram and logic

---
+++ Assets/Scripts/CameraPuzzle/ShowCameraDiagram.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class ShowCameraDiagram : MonoBehaviour
+{
+    [SerializeField] private GameObject cameraDiagram;
+    [SerializeField] private Animator diagramAnimator;
+    private bool diagramOpen = false;
+
+    void Start()
+    {
+        cameraDiagram.SetActive(false);
+    } 
+
+    public void ShowDiagramCamera()
+    {
+        cameraDiagram.SetActive(true);
+        diagramOpen = true;
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Interact") && diagramOpen)
+        {
+            cameraDiagram.SetActive(false);
+            diagramOpen = false;
+            diagramAnimator.SetTrigger("CloseDiagram");
+        }
+    }
+}
