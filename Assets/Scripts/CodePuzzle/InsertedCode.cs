142a72cbac142c1af942827af1cc53e9effaad1e
bernabarros <bernardobarros.work@gmail.com>
2025-12-15 23:31:26 +0000

    Implement code puzzle, placed camera puzzle diagram on panel door

--- Assets/Scripts/CodePuzzle/InsertedCode.cs
+++ Assets/Scripts/CodePuzzle/InsertedCode.cs
@@ -1,22 +1,68 @@
+using System.Linq;
 using UnityEngine;
 
 public class InsertedCode : MonoBehaviour
 {
-    public int[] codeNumbers = new int[3];
+    private int[] codeNumbers = new int[3];
+    private int[] correctCode = new int[] { 3, 1, 5 };
+    private int currentIndex = 0;
+    private bool isCorrect = false;
+    private bool isComplete = false;
     Interactive interactive;
 
     void Start()
     {
         interactive = GetComponent<Interactive>();
+
+        interactive.interactiveData.requirementsMessage = "";
     }
 
     void Update()
     {
-        
+        isComplete = IsComplete();
+        if(isComplete)
+        {
+            isCorrect = CheckCode();
+
+            if(isCorrect)
+            {
+                interactive.FulfillRequirements();
+                interactive.TriggerIndirect();
+            }
+            else if(!isCorrect)
+            {
+                codeNumbers = new int[3];
+                interactive.interactiveData.requirementsMessage = "";
+                isComplete = false;
+            }
+        }
     }
 
     public void InsertNumber(int codeNumber)
     {
-        interactive.interactiveData.requirementsMessage += $"{codeNumber} ";
+        codeNumbers[currentIndex] = codeNumber;
+        currentIndex++;
+
+        interactive.interactiveData.requirementsMessage = GetCodeAsString();
+    }
+
+    private string GetCodeAsString()
+    {
+        string result = "";
+
+        for(int i = 0; i < currentIndex; i++)
+        {
+            result += codeNumbers[i] + " ";
+        }
+        return result;
+    }
+    public bool IsComplete()
+    {
+        return currentIndex == codeNumbers.Length;
+    }
+
+    public bool CheckCode()
+    {
+        return codeNumbers.SequenceEqual(correctCode);
     }
 }
