f3e08368004efd76d02bcbf7c346862ade3dc6a2
bernabarros <bernardobarros.work@gmail.com>
2026-01-03 22:44:29 +0000

    Add sound to book opening and closing

--- Assets/Scripts/CodePuzzle/ShowBookContent.cs
+++ Assets/Scripts/CodePuzzle/ShowBookContent.cs
@@ -4,9 +4,17 @@ public class ShowBookContent : MonoBehaviour
 {
     [SerializeField] private GameObject bookContent;
     [SerializeField] private Animator contentAnimator;
+    [SerializeField] private AudioClip openBook;
+    [SerializeField] private AudioClip closeBook;
+    private AudioSource audioSource;
     private bool bookOpen = false;
 
     public static bool isReading = false;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     
     void Start()
     {
@@ -15,6 +23,7 @@ public class ShowBookContent : MonoBehaviour
 
     public void ShowContentBook()
     {
+        audioSource.PlayOneShot(openBook);
         bookContent.SetActive(true);
         bookOpen = true;
         isReading = true;
@@ -24,6 +33,7 @@ public class ShowBookContent : MonoBehaviour
     {
         if(Input.GetButtonDown("Interact") && bookOpen)
         {
+            audioSource.PlayOneShot(closeBook);
             bookContent.SetActive(false);
             bookOpen = false;
             contentAnimator.SetTrigger("Close Content");
